34118289edf361ee167ff0a638484efb1c7e8e99
alokita-tech <badamealokita7@gmail.com>
2025-02-24 11:15:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 
-API_KEY = "ee78c65eec983eae43cb24e6f1700a8b"  # Replace with your actual API key
+API_KEY = "ee"  # Replace with your actual API key
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
